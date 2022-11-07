608e5c2374930d2773b406ae8443cfcb4630b136
Tutialfred <106350481+Tutialfred@users.noreply.github.com>
2022-11-07 17:59:48 -0300

    working

--- api/src/models/Tipo.js
+++ api/src/models/Tipo.js
@@ -2,21 +2,16 @@ const { DataTypes, Model } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 
-class pokemons extends Model {}
+class Tipo extends Model {}
 
 module.exports = (sequelize) => {
   // defino el modelo
-  pokemon.init({
+  Tipo.init({
     id: {type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, allowNull:false, primaryKey: true },
     // UUID → Te genera un numero randon con letras y numeros unico y no se puede repetir
 
     name: {type: DataTypes.STRING, allowNull: false,},
-    life: {type: DataTypes.STRING},
-    attack: {type: DataTypes.STRING},
-    defense: {type: DataTypes.STRING},
-    speed: {type: DataTypes.STRING},
-    height: {type: DataTypes.STRING},
-    weight: {type: DataTypes.STRING},
+
     createInData: {type: DataTypes.BOOLEAN, allowNull: false, defaultValue: true}
   });
-};
+}
